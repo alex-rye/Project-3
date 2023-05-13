95c034d14d59bd09b3877b576c8d7c7b18f3cf0a
Alex Rye <alexrye96@gmail.com>
2023-05-13 15:08:31 -0400

    added mapping for array in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import './App.css'
+import restaurantArray from './restaurant.json'
 
 const App = () => {
-  return <div>My Project</div>
+  return (
+    <div className="App">
+      <h1 className="header">Best Restaurants in Columbus, Ohio</h1>
+      <main>
+        {restaurantArray.map((restaurant) => (
+          <Restaurant restaurant={restaurant} key={restaurant.name} />
+        ))}
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Restaurant.jsx
@@ -0,0 +1,9 @@
+const Restaurant = (props) => {
+  return (
+    <div>
+      <img src={props.photo} alt={props.name} />
+    </div>
+  )
+}
+
+export default Restaurant
